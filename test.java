7f4a9694521f481425b23f24028a689f42045c56
Gudibanda-Suresh <109528504+Gudibanda-Suresh@users.noreply.github.com>
2022-12-25 22:21:53 +0530

    am add j

--- test.java
+++ test.java
@@ -4,7 +4,7 @@ public class test{
         int i,j,num=1;
         for (i=1;i<=5;i++){
             for (j=1;j<=i;j++){
-                System.out.print((num)+" j");
+                System.out.print((num)+" jj");
                 num++;
             }
             System.out.println();
